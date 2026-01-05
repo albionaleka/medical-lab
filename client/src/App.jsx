6fb9e2cb8346700e09af071a7a65669888fe0cb6
Albiona Leka <albioneleka@gmail.com>
2026-01-05 01:17:17 +0100

    feat(ui): add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,101 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext, useState } from "react";
+import { IoMail } from "react-icons/io5";
+import { IoIosLock } from "react-icons/io";
+
+const Login = () => {
+  const { login } = useContext(AuthContext);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(formData.email, formData.password);
+  };
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="flex min-h-screen bg-gray-50 items-center justify-center px-4">
+      <div className="flex flex-col items-center justify-center p-6 bg-white shadow-md rounded-xl border border-gray-200 hover:shadow-lg transition-shadow duration-300">
+        <form
+          className="md:w-96 w-80 flex flex-col items-center justify-center"
+          onSubmit={handleSubmit}
+        >
+          <h2 className="text-4xl text-gray-900 font-medium">Sign in</h2>
+          <p className="text-sm text-gray-500/90 mt-3">
+            Welcome back! Please sign in to continue
+          </p>
+
+          <div className="flex items-center gap-4 w-full my-5">
+            <div className="w-full h-px bg-gray-300/90"></div>
+            <p className="w-full text-nowrap text-sm text-gray-500/90">
+              Sign in with email
+            </p>
+            <div className="w-full h-px bg-gray-300/90"></div>
+          </div>
+
+          <div className="flex items-center w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
+            <IoMail size={20} color="#6B7280" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={onChange}
+              className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full"
+              required
+            />
+          </div>
+
+          <div className="flex items-center mt-6 w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
+            <IoIosLock size={20} color="#6B7280" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={onChange}
+              className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full"
+              required
+            />
+          </div>
+
+          <div className="w-full flex items-center justify-between mt-8 text-gray-500/80">
+            <div className="flex items-center gap-2">
+              <input className="h-5" type="checkbox" id="checkbox" />
+              <label className="text-sm" htmlFor="checkbox">
+                Remember me
+              </label>
+            </div>
+            <a className="text-sm underline" href="/reset-password">
+              Forgot password?
+            </a>
+          </div>
+
+          <button
+            type="submit"
+            className="mt-8 w-full h-11 rounded-full text-white bg-green-500 hover:opacity-90 transition-opacity"
+          >
+            Login
+          </button>
+          <p className="text-gray-500/90 text-sm mt-4">
+            Don’t have an account?{" "}
+            <a className="text-green-400 hover:underline" href="#">
+              Sign up
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
