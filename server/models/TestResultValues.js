762401e66410e2a9e293841d3ba6f5f6d03cc40a
Albiona Leka <albioneleka@gmail.com>
2026-01-12 23:06:05 +0100

    feat(models): add TestResultValues sequelize model

---
+++ server/models/TestResultValues.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const TestResultValues = sequelize.define(
+  "TestResultValues",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    testResultId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    parameterId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    resultValue: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    resultStatus: {
+      type: DataTypes.ENUM("LOW", "NORMAL", "HIGH"),
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "test_result_values",
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+export default TestResultValues;
