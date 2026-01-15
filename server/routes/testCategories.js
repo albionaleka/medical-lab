3aec45849a6ec462eedbdd97eeba6a84496993ae
Albiona Leka <albioneleka@gmail.com>
2026-01-15 23:26:12 +0100

    feat(categories): restrict test categories routes to authorized roles

--- server/routes/testCategories.js
+++ server/routes/testCategories.js
@@ -1,12 +1,32 @@
 import { TestCategoryController } from "../controllers/TestCategoryController.js";
 import express from "express";
+import { authenticate, authorize } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/", TestCategoryController.createCategory);
-router.get("/", TestCategoryController.getAllCategories);
-router.get("/:id", TestCategoryController.getCategoryById);
-router.put("/:id", TestCategoryController.updateCategory);
-router.delete("/:id", TestCategoryController.deleteCategory);
+router.post(
+  "/",
+  authenticate,
+  authorize("ADMIN"),
+  TestCategoryController.createCategory
+);
+
+router.get("/", authenticate, TestCategoryController.getAllCategories);
+
+router.get("/:id", authenticate, TestCategoryController.getCategoryById);
+
+router.put(
+  "/:id",
+  authenticate,
+  authorize("ADMIN"),
+  TestCategoryController.updateCategory
+);
+
+router.delete(
+  "/:id",
+  authenticate,
+  authorize("ADMIN"),
+  TestCategoryController.deleteCategory
+);
 
 export default router;
