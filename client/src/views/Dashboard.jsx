d5300fb1e5796d4aeba93c6087d927b6bda35274
Albiona Leka <albioneleka@gmail.com>
2026-01-17 01:54:48 +0100

    feat: configure role based dashboard

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,11 +1,33 @@
+import { useContext } from "react";
 import Layout from "../components/Layout/Layout";
+import { AuthContext } from "../context/AuthContext";
+import AdminDashboard from "../components/Dashboard/AdminDashboard";
+import DoctorDashboard from "../components/Dashboard/DoctorDashboard";
+import LaborantDashboard from "../components/Dashboard/LaborantDashboard";
+import { ROLES } from "../utils/roles";
 
 const Dashboard = () => {
-  return (
-    <Layout>
-      <div>Dashboard</div>
-    </Layout>
-  );
+  const { loggedIn } = useContext(AuthContext);
+  const user = JSON.parse(localStorage.getItem("user") || "null");
+
+  if (!loggedIn || !user) {
+    return null;
+  }
+
+  const renderDashboard = () => {
+    switch (user.role) {
+      case ROLES.ADMIN:
+        return <AdminDashboard />;
+      case ROLES.DOCTOR:
+        return <DoctorDashboard />;
+      case ROLES.LABORANT:
+        return <LaborantDashboard />;
+      default:
+        return <div>Unknown role</div>;
+    }
+  };
+
+  return <Layout title="Dashboard">{renderDashboard()}</Layout>;
 };
 
 export default Dashboard;
