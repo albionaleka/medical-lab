9e001499ff73bcc97a4b9a46eec1ce7665f20cc8
Albiona Leka <albioneleka@gmail.com>
2026-01-20 23:19:27 +0100

    feat: redirect to dashboard if user is logged in

--- client/src/views/GetStarted.jsx
+++ client/src/views/GetStarted.jsx
@@ -1,7 +1,16 @@
 import Navbar from "../components/Navbar";
 import Start from "../components/Start";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 
 const GetStarted = () => {
+  const { loggedIn } = useContext(AuthContext);
+
+  if (loggedIn) {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
     <>
       <Navbar />
